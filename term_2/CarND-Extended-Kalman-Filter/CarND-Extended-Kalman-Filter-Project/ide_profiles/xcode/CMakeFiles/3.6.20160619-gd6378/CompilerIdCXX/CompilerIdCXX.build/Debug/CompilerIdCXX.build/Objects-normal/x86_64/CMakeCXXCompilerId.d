6ba6401e44b4106ed4a6898fec52c021130c48cb
Peter Huang <hbd730@gmail.com>
2017-04-18 21:38:00 +1000

    remove submodule

---
+++ term_2/CarND-Extended-Kalman-Filter/CarND-Extended-Kalman-Filter-Project/ide_profiles/xcode/CMakeFiles/3.6.20160619-gd6378/CompilerIdCXX/CompilerIdCXX.build/Debug/CompilerIdCXX.build/Objects-normal/x86_64/CMakeCXXCompilerId.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/peterh/self-driving-car-nd/term_2/CarND-Extended-Kalman-Filter/CarND-Extended-Kalman-Filter-Project/ide_profiles/xcode/CMakeFiles/3.6.20160619-gd6378/CompilerIdCXX/CMakeCXXCompilerId.cpp
---
+++ term_2/CarND-Extended-Kalman-Filter/CarND-Extended-Kalman-Filter-Project/src/kalman_filter.cpp
@@ -0,0 +1,71 @@
+#include "kalman_filter.h"
+#include <iostream>
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+KalmanFilter::KalmanFilter() {}
+
+KalmanFilter::~KalmanFilter() {}
+
+void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
+                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
+  x_ = x_in;
+  P_ = P_in;
+  F_ = F_in;
+  H_ = H_in;
+  R_ = R_in;
+  Q_ = Q_in;
+}
+
+void KalmanFilter::Predict() {
+	x_ = F_ * x_;
+	MatrixXd Ft = F_.transpose();
+	P_ = F_ * P_ * Ft + Q_;
+}
+
+void KalmanFilter::Update(const VectorXd &z) {
+	VectorXd z_pred = H_ * x_;
+	VectorXd y = z - z_pred;
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si;
+	
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
+}
+
+void KalmanFilter::UpdateEKF(const VectorXd &z) {
+	float px, py, vx, vy;
+	px = x_[0];
+	py = x_[1];
+	vx = x_[2];
+	vy = x_[3];
+	
+	float rho_predict = sqrt(px*px + py*py);
+	if(fabs(px) < 0.0001 || fabs(rho_predict) < 0.0001)
+	{
+		std::cerr << "UpdateEKF () - Error - Division by Zero" << std::endl;
+		return;
+	}
+	
+	VectorXd hx(3);
+ 	hx << rho_predict, atan2(py,px), (px*vx + py*vy)/rho_predict;
+	VectorXd y = z - hx;
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si;
+	
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
+}

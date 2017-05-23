4fe70e5abf5e27c49992095d1d8d92fd921b923b
Peter Huang <hbd730@gmail.com>
2017-05-24 00:27:17 +1000

    add reflection

--- term_2/CarND-PID-Control-Project/src/PID.h
+++ term_2/CarND-PID-Control-Project/src/PID.h
@@ -1,18 +1,21 @@
 #pragma once
 
 #include <algorithm>
+/*
+   Effect of each component
+   1. P term describe how much the system response to the error propotianally.
+   2. I term can increase the settle time, but it coul dovershoot if toolarge. It is mainly used to eliminate steady state error. i.e system can not reach setpoint for some reasons (vehicle wheel not calibrated well, a drone need more accumulated thrust to reach hover setpoint as battery power is decreasing. etc.
+   3. D term is prediction. It is the error's change rate which conteracts with the effect of P and I. it can effectively reduce oscillation,      It is like a spring or an inductor which opposes the rate of change of current flowing through it. That is why it can achieve smooth steering control. However, too large cause overdamped system, slower settle time, and cause system to chatter.
+*/
 
-/* To tune a PID use the following steps:
-1. Set all gains to zero.
-2. Increase the P gain until the response to a disturbance is steady oscillation.
-3. Increase the D gain until the the oscillations go away (i.e. it's critically damped).
-4. Repeat steps 2 and 3 until increasing the D gain does not stop the oscillations.
-5. Set P and D to the last stable values.
-6. Increase the I gain until it brings you to the setpoint with the number of oscillations desired (normally zero but a quicker response can be had if you don't mind a couple oscillations of overshoot)
- 
-What disturbance you use depends on the mechanism the controller is attached to. Normally moving the mechanism by hand away from the setpoint and letting go is enough. If the oscillations grow bigger and bigger then you need to reduce the P gain.
-																																						 
-If you set the D gain too high the system will begin to chatter (vibrate at a higher frequency than the P gain oscillations). If this happens, reduce the D gain until stops.
+/* The following steps are used to tune PID:
+1. Determine control frequency which is used to calculate time step dt = 1/frequency. Control frequency depends on system's dynamics, this is usually determined empirically. In this simulator, it is measured to be 80Hz.
+2. Set all gains to zero to start with.
+3. Increase the P gain until the car starts oscillates around the expected path (center of the road).
+4. Increase the D gain until the the oscillations goes away (i.e. it's critically damped).
+5. Repeat steps 2 and 3 until increasing the D gain does not stop the oscillations.
+6. Set P and D to the last stable values.
+7. Increase the I gain until it brings you the the set point if there is steady state error. In simulation there isn't, but it can be used to increase response time to settle.) PD is enough to have a good drive on the entire track already. 
 */
 
 template <typename T>
